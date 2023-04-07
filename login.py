6ad2686f3d5aeaecddc363c89e323bc217b0b057
= <indrswara@gmail.com>
2023-04-07 16:26:51 +0700

    Login

--- login.py
+++ login.py
@@ -1,35 +1,37 @@
 import csv 
 from OurFunction import *
 
-login_status = False
-not_found = False
-f = open('user.csv', 'r+')
-reader = csv.reader(f, delimiter=";")
+def Login():
+    login_status = False
+    not_found = False
+    f = open('user.csv', 'r+')
+    reader = csv.reader(f, delimiter=";")
 
+    username = input("Username: ")
+    password = input("Password: ")
+    
+    #check username ada atau tidak
+    username_check = []
+    for row in reader: 
+        username_check = appends(username_check,row[0])
+        if row[0] == username and row[1] == password: 
+                login_status = True
+                role = row[2]
 
-username = input("Username: ")
-password = input("Password: ")
+    for i in range(Length(username_check)): 
+        if username == username_check[i] : 
+            not_found = False
+            break
+        else: 
+            not_found = True
 
-#check username ada atau tidak
-username_check = []
-for row in reader: 
-    username_check = appends(username_check,row[0])
-    if row[0] == username and row[1] == password: 
-            login_status = True
+    if not_found == True: 
+        print("username tidak terdaftar")
+    else:
+        if login_status == False: 
+            print("Password salah")
+        else: 
+            print("Anda benar")
+            return role
 
-
-for i in range(Length(username_check)): 
-    if username == username_check[i] : 
-        not_found = False
-        break
-    else: 
-        not_found = True
-
-if not_found == True: 
-    print("username tidak terdaftar")
-else:
-    if login_status == False: 
-        print("Password salah")
-    else: 
-        print("Anda benar")
 
--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 #Program utama ada disini
-import login 
+from login import *
+from helps import *
+import os
 
-login()
+
+role = ""
+while True: 
+    masukkan = input(">>> ")
+    if masukkan == "login" and role == "": 
+        role = Login()
+    if masukkan == "logout": 
+        role = ""
+    if role == "jin_pembangun" and masukkan == "bagun": 
+        print()
+    if role == "jin_pengumpul" and masukkan == "bruh":
+        print()
+    if role == "roro_jonggrang":
+        print()
+    if masukkan == "help": 
+        helps()
+    if masukkan == "exit": 
+        exit()
+    print(role)
+    
