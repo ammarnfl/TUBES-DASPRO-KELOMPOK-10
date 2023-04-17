9f2644173033cc8c3842d0b34a6622e2b3c94820
rossiptr <118780416+rossiptr@users.noreply.github.com>
2023-04-17 22:08:19 +0700

    Update F16_Exit.py

--- F16_Exit.py
+++ F16_Exit.py
@@ -2,14 +2,15 @@ from F01_Login import *
 from F14_Save import *
 from Z1_ListFunction import *
 
-def exit( ):
+def exit(array_of_candi : list, array_of_user : list, array_of_material: list):
     while login_status == True:
         simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-
+        folder_input = input("Masukkan nama folder: ")
+        username = input("Masukkan username jin: ")
         if simpan == 'n' or simpan == 'N':
             exit
         elif simpan == 'y' or simpan == 'Y':
-            save()
+            save(folder_input, 'user.csv', username)
         else:
             simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
     return exit        
