0836e0f46b62c59a21c593e382da6555a96c069c
rossiptr <118780416+rossiptr@users.noreply.github.com>
2023-04-17 11:07:35 +0700

    Create F12_AyamBerkokok.py

---
+++ F12_AyamBerkokok.py
@@ -0,0 +1,15 @@
+from Z1_ListFunction import * 
+
+def AyamBerkokok (Total_candi: int) -> str:
+    print("Kukuruyuk.. Kukuruyuk..")
+    print(f"Jumlah candi: {Total_candi}")
+
+    if Total_candi >= 100:
+        print("Yah, Bandung Bondowoso memenangkan permainan!")
+    
+    else:
+        print("Selamat, Roro Jonggrang memenangkan permainan!")
+        print("")
+        print("*Bandung Bondowoso angry noise*")
+        print("Roro Jonggrang dikutuk menjadi candi.")
+    exit
