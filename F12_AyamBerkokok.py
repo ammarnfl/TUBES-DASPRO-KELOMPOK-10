6315e7e368ca7a4d4c13e3cfe5cd4e66d704b0cb
= <indrswara@gmail.com>
2023-04-20 16:35:17 +0700

    Memperbaiki F12_ayam berkokok

--- F12_AyamBerkokok.py
+++ F12_AyamBerkokok.py
@@ -1,12 +1,13 @@
 from Z1_ListFunction import * 
 from typing import Any
+
 def AyamBerkokok (array_of_candi: list) -> Any:
     print("Kukuruyuk.. Kukuruyuk..")
     count_candi = 0
-    for i in range(1, Len(array_of_candi)): 
+    for i in range(0, Len(array_of_candi)): 
         if Len(array_of_candi[i]) != 1: 
             count_candi += 1
-    
+    count_candi -= 1
     print(f"Jumlah candi: {count_candi}")
 
     if count_candi >= 100:
