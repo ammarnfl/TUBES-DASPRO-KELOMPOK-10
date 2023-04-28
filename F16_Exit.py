9abaa672894b62623bbae2c69bcd1439ca32b526
ammarnfl <117100501+ammarnfl@users.noreply.github.com>
2023-04-28 11:00:52 +0700

    benerin exit

--- F16_Exit.py
+++ F16_Exit.py
@@ -1,21 +1,8 @@
 from F14_Save import *
 from Z1_ListFunction import *
 
-def exit(array_of_candi : list, array_of_user : list, array_of_material: list):
-    while login_status == True:
-        simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-        folder_input = input("Masukkan nama folder: ")
-        username = input("Masukkan username jin: ")
-        if simpan == 'n' or simpan == 'N':
-            exit
-        elif simpan == 'y' or simpan == 'Y':
-            save(folder_input, 'user.csv', username)
-        else:
-            simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    return exit        
-
 def Exit(array_of_user : list, array_of_candi :list , array_of_material : list):
-    simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (Y/N) ")
+    simpan = input("\nApakah Anda mau melakukan penyimpanan file yang sudah diubah (Y/N)?\n")
 
     if simpan == 'n' or simpan == 'N':
         exit()
@@ -27,5 +14,5 @@ def Exit(array_of_user : list, array_of_candi :list , array_of_material : list):
         save(folder, array_of_user, 'user.csv')
         save(folder, array_of_candi, 'candi.csv')
         save(folder, array_of_material, 'bahan_bangunan.csv')
-        print(f"Menyimpan folder di {folder}...")
+        print(f"Menyimpan folder di {folder}...\n")
     exit()
