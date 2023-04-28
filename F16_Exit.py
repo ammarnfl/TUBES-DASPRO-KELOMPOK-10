65593a74733127e476e4bc4d6ff3093831e8e0e3
ammarnfl <ammarrnfl@gmail.com>
2023-04-28 11:37:51 +0700

    nambah narasi exit

--- F16_Exit.py
+++ F16_Exit.py
@@ -6,6 +6,7 @@ def Exit(array_of_user : list, array_of_candi :list , array_of_material : list):
 
     while True: 
         if simpan == 'n' or simpan == 'N':
+            print("\nKeluar dari program...\n")
             exit()
         elif simpan == 'y' or simpan == 'Y':
             parent = "data"
@@ -16,6 +17,7 @@ def Exit(array_of_user : list, array_of_candi :list , array_of_material : list):
             save(folder, array_of_candi, 'candi.csv')
             save(folder, array_of_material, 'bahan_bangunan.csv')
             print(f"Menyimpan folder di {folder}...\n")
+            print("Keluar dari program...\n")
             exit()
         print("Input tidak sesuai")
         simpan = input("\nApakah Anda mau melakukan penyimpanan file yang sudah diubah (Y/N)?\n")
